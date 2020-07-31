a5f11ebc65f2dc6fd728d2d309427a1b7d8cc813
lowgravity1203 <matthew.f.barnes1203@gmail.com>
2020-07-31 12:29:15 -0700

    doc: add chatroom model, interest model, post model, and added to user model

---
+++ models/chatroom.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const Interest = require('./interest')
+const Post = require('./post')
+
+const chatroomSchema = new mongoose.Schema({
+    interest: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Interest'  
+    }],
+    post: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }]
+
+})
+
+const Chatroom = mongoose.model('Chatroom', chatroomSchema)
+
+module.exports = Chatroom
--- models/user.js
+++ models/user.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose")
 const passportLocalMongoose = require("passport-local-mongoose")
-
+const Interest = require('./interest')
 
 const UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String, 
+    displayName: String,
+    email: String,
+    interests: [{
+        type: mongoose.Schema.Types.ObjectId
+
+    }]
 })
+
 
 UserSchema.plugin(passportLocalMongoose)
 
