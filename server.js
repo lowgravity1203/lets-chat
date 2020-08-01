bf595664b867ac7a16361defd2407b481e9e5f69
Manny <hi@mannyparra.com>
2020-08-01 14:32:30 -0600

    docs: add dependenceis express-ejs-layouts, node-saas; update scripts npm run dev

--- server.js
+++ server.js
@@ -1,59 +1,53 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const methodOverride = require("method-override")
-const bodyParser = require("body-parser")
-const passport = require("passport")
-const LocalStrategy = require("passport-local")
-const User = require("./models/user")
-
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
+const expressLayouts = require('express-ejs-layouts')
 
 // Requiring routes
-const indexRoutes = require("./routes/index")
-const channelRoutes = require("./routes/channels")
-const Interest = require("./models/interest")
+const indexRoutes = require('./routes/index')
+const channelRoutes = require('./routes/channels')
+const Interest = require('./models/interest')
 
 // Require database info from file
 require('./db/db')
 
 //PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "anything can go here",
-	resave: false,
-	saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use(
+  require('express-session')({
+    secret: 'anything can go here',
+    resave: false,
+    saveUninitialized: false,
+  }),
+)
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 // Use static file for css, urlencoded for req.body, and methodOverride
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'))
 app.use(bodyParser.json())
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
 
 //A MIDDLEWARE FOR EVERY ROUTE IN ORDER TO REQ.USER
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user;
-	next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user
+  next()
 })
-
 
 // Use routes
-app.use("/", indexRoutes)
-app.use("/channel", channelRoutes)
+app.use('/', indexRoutes)
+app.use('/channel', channelRoutes)
 
-app.listen(3000, ()=>{
-    console.log("app is listening on port 3000")
+app.listen(3000, () => {
+  console.log('app is listening on port 3000')
 })
-
-
-
-
-
-
-
-	
-
