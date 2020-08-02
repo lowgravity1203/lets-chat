a192e798492ef7bc54d1d5d7499e164a91d4aac4
lowgravity1203 <matthew.f.barnes1203@gmail.com>
2020-08-01 19:22:50 -0700

    adds interests to database, but only locally, all commented out

--- routes/channels.js
+++ routes/channels.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const route = express.Router()
 const User = require("../models/user")
+const Post = require('../models/post')
+
+// let ev = User.findOne({username: "ev"})
+// console.log(ev.id)
+
+// let posts = { 
+//     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+//     date: 2018-09-19,
+//     tage: "dolor sit amet"
+//  }
+
+Post.create(posts, (err, createdPost)=> {
+    if(err){
+        console.log(err)
+    } else{
+        console.log(createdPost)
+    }
+})
 
 // Get index route
 route.get("/", (req, res) => {
---
+++ seed.js
@@ -0,0 +1,13 @@
+// const mongoose = require("mongoose")
+// const user = require("./models/user")
+// Post = require('./models/post')
+// User = require('./models/user')
+
+
+// let ev = User.findOne({username: "ev"})
+// coneole.log('ev')
+
+// let posts = [{ 
+//     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//     user: ""  
+//  }]
