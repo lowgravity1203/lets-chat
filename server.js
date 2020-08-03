0629ca8cd5f309a53c5b5aed142e1d7c830fefa8
lowgravity1203 <matthew.f.barnes1203@gmail.com>
2020-08-03 11:11:25 -0700

    setup atlas and dotenv

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
-const app = express()
 const methodOverride = require("method-override")
 const bodyParser = require("body-parser")
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
 const User = require("./models/user")
+const mongoose = require("mongoose")
+const dotenv = require('dotenv').config()
+const app = express()
 
 
 // Requiring routes
@@ -12,10 +14,16 @@ const indexRoutes = require("./routes/index")
 const channelRoutes = require("./routes/channels")
 const Interest = require("./models/interest")
 
-// Require database info from file
-require('./db/db')
+//database connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log('Connected Database Successfully')
+})
 
 //PASSPORT CONFIGURATION
+
 app.use(require("express-session")({
 	secret: "anything can go here",
 	resave: false,
