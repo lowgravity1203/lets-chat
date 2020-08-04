6696302c5c9c80643bbb01ac79257d6be9c3f33e
Manny <hi@mannyparra.com>
2020-08-03 19:31:13 -0600

    fix: resolve git conflict

--- models 2/interest.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const interestSchema = new mongoose.Schema({
-  name: String,
-})
-
-const Interest = mongoose.model('Interest', interestSchema)
-
-module.exports = Interest
