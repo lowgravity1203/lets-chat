2609a7962f04ea3514055edbfb3e7d34bd7e615f
Evan Doherty <edoherty77@gmail.com>
2020-08-07 12:50:30 -0400

    updated route

--- routes/posts.js
+++ routes/posts.js
@@ -106,14 +106,15 @@ router.put("/:channel/:post_id/edit", (req, res) => {
 
 // Display form to reply to post
 router.get("/:channel/:post_id/reply", (req, res) => {
-  Channel.findOne({name: req.params.channel}, (err, foundChannel)=> {
-    if(err)console.log(err)
-    Post.findById(req.params.post_id, (err, foundPost)=> {
-      if(err)console.log(err)
-      res.render("posts/reply", {currentChannel: foundChannel, post: foundPost})
-    })
-  })
+        Channel.findOne({name: req.params.channel}, (err, foundChannel)=> {
+                if(err)console.log(err)
+                Post.findById(req.params.post_id, (err, foundPost)=> {
+                        if(err)console.log(err)
+                        res.render("posts/reply", {currentChannel: foundChannel, post: foundPost})
+                })
+        })
 })
+
 
 router.post("/:channel/:post_id/reply", (req, res) => {
   Post.findById(req.params.post_id, (err, foundPost) => {
